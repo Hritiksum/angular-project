f71e07de8eda757bbdfbf0b7866e317bb455c4e1
hritiksum <hritik.sum@gmail.com>
2021-07-30 00:17:28 +0530

    lading page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
   {
     path:'',
+    component: LandingPageComponent
+  },
+  {
+    path:'account',
     loadChildren:() => import('./account/account.module').then(m => m.AccountModule)
   }
 ];
