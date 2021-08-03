1f5d3940961f1de80e0364752349001afa8b57ff
hritiksum <hritik.sum@gmail.com>
2021-08-04 00:06:16 +0530

    login n logout added

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ export class AuthService{
             (resolve, reject) => {
                 setTimeout(()=>{
                     resolve(this.loggedIn);
-                },8000)
+                },800)
             }
         );
         return promise;
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { faIgloo } from '@fortawesome/free-solid-svg-icons';
 
+import { AuthService } from 'src/app/services/auth.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -9,10 +11,19 @@ import { faIgloo } from '@fortawesome/free-solid-svg-icons';
 export class HeaderComponent implements OnInit {
   // fontawesome: Tie the icon to the property in your component
   faigloo = faIgloo;
+  logStatus:boolean = false;
 
-  constructor() { }
+  constructor(private authservice: AuthService) { }
 
   ngOnInit(): void {
+  }
+  login(){
+    this.authservice.login();
+    this.logStatus=this.authservice.loggedIn;
+  }
+  logout(){
+    this.authservice.logout();
+    this.logStatus=this.authservice.loggedIn;
   }
 
 }
